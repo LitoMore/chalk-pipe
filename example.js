81cc9627b9629a3b993e5d686596cc203f72509b
LitoMore <litomore@gmail.com>
2017-09-08 10:07:07 +0800

    minor fix

--- example.js
+++ example.js
@@ -3,11 +3,11 @@
 const chalk = require('chalk');
 const chalkPipe = require('.');
 
-const fakeChalk = new chalk.constructor({enabled: false})
+const fakeChalk = new chalk.constructor({enabled: false});
 
 console.log();
 console.log(chalkPipe('dim.#ffffff')('dim.#ffffff'));
 console.log(chalkPipe('cyan.underline')('cyan.underline'));
 console.log(chalkPipe('orange.bold')('orange.bold'));
 console.log(chalkPipe('bgCyan.black')('bgCyan.black'));
-console.log(chalkPipe('bgCyan.black', fakeChalk)('bgCyan.black'))
+console.log(chalkPipe('bgCyan.black', fakeChalk)('bgCyan.black'));
