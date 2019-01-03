5b9313879b30a83b3e6d6feacff83f66a7bbcb59
LitoMore <litomore@gmail.com>
2019-01-04 04:02:27 +0800

    Add tests

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 
 const test = require('ava');
 const chalk = require('chalk');
+const cssColorNames = require('css-color-names');
 
 const {modifiers, normalColors} = require('./styles');
 const chalkPipe = require('.');
@@ -48,6 +49,14 @@ test('Normal Colors', t => {
 	t.is(text, should);
 });
 
+test('Keyword styles', t => {
+	const scheme = chalkPipe('hotpink', chalk);
+	const text = scheme('hug');
+	const should = chalk.hex(cssColorNames.hotpink)('hug');
+
+	t.is(text, should);
+});
+
 test('Background styles', t => {
 	const scheme = chalkPipe('bgRed.bg#ff99cc.bgPink.bgBlackBright', chalk);
 	const text = scheme('unicorn');
@@ -56,6 +65,38 @@ test('Background styles', t => {
 		.bgHex('#ff99cc')
 		.bgKeyword('pink')
 		.bgBlackBright('unicorn');
+
+	t.is(text, should);
+});
+
+test('Hex color', t => {
+	const scheme = chalkPipe('#ff99cc', chalk);
+	const text = scheme('flora');
+	const should = chalk.hex('#ff99cc')('flora');
+
+	t.is(text, should);
+});
+
+test('Empty style', t => {
+	const scheme = chalkPipe('', chalk);
+	const text = scheme('lito');
+	const should = chalk('lito');
+
+	t.is(text, should);
+});
+
+test('Unknown style', t => {
+	const scheme = chalkPipe('unknown', chalk);
+	const text = scheme('sasuke');
+	const should = chalk('sasuke');
+
+	t.is(text, should);
+});
+
+test('Built-in Chalk', t => {
+	const scheme = chalkPipe();
+	const text = scheme('normal text');
+	const should = 'normal text';
 
 	t.is(text, should);
 });
